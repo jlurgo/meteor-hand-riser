b2c9b2c3b68d8d79ddaca8cea8acafc0a309b30b
jurgo <jurgo@ekumenlabs.com>
2018-12-04 14:13:40 -0300

    only google authentication

--- server/main.js
+++ server/main.js
@@ -2,5 +2,12 @@ import { Meteor } from 'meteor/meteor';
 
 import '../imports/api/turnsToSpeak.js';
 Meteor.startup(() => {
-  // code to run on server at startup
+// code to run on server at startup
+});
+
+Accounts.validateNewUser(function(user){
+  if(user.services.google.email.match(/creativa77\.com\.ar$/)) {
+      return true;
+  }
+  throw new Meteor.Error(403, "You must sign in using a creativa77.com.ar account");
 });
